48621c8367f0971efc2f8c4c7984b5cfb0ee87fe
KumJungMin <rmawjdals@naver.com>
2023-06-19 17:23:07 +0900

    1. github api로 리뷰할당 현황 접근하기

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
-import "dotenv/config";
-import { Webhook, MessageBuilder } from "discord-webhook-node";
 import PRAssign from "./msg/pr-assign.js";
 
-const hook = new Webhook(process.env.HOOK_URL);
-
-async function sendMessage() {
+async function init() {
   const msg = await PRAssign.getMessage();
-  const embed = new MessageBuilder()
-    .setTitle("오늘자 리뷰 할당 현황")
-    .setColor("#00ff00")
-    .setDescription(
-      "🌞 굿모닝 좋은 아침 🌞\n💪 오늘자 리뷰 할당 현황 알려드려요!"
-    )
-    .addField("할당 현황", msg)
-    .addField(
-      "😉 유의사항",
-      "* 당일 오전 9시 이전에 요청한 리뷰는 당일 오후 2시까지 리뷰 완료해주세요!\n* 리뷰가 늦어질 시 리뷰 요청자에게 DM으로 미리 알려주는 센스~!\n* 일부 인원에게 많은 리뷰가 가지 않게 골고루 리뷰를 걸어주세요"
-    );
-
-  hook.send(embed);
+  console.log(msg);
 }
 
-sendMessage();
+init();
